0754400d91db20737f3eb6fe520705cfdac40e25
Manpreet-Singh-1 <mr.mps9999@gmail.com>
2024-06-05 01:47:28 +0530

    Day-3

---
+++ Python/Numpy_/11_Sorted_array.py
@@ -0,0 +1,30 @@
+# sort() - the numpy ndarray object has a function which is called sort(), and this will sort a specified array.
+# import numpy as np
+# a = np.array([3,2,0,1])
+# b=np.sort(a)
+# print(b) # this method is like the copy()
+# print(a)
+# import numpy as np
+# a = np.array([3,2,0,1])
+# b=np.sort(a)[::-1]
+# print(b) # this method is like the copy()
+# print(a)
+
+# sort the array alphabetically
+# import numpy as np
+# sharad = np.array(['banana', 'cherry', 'apple'])
+# print(np.sort(sharad))
+
+# import numpy as np
+# sharad = np.array(['banana', 'cherry', 'apple'])
+# print(np.sort(sharad)[::-1])
+
+# sort the boolean array:
+# import numpy as np
+# sharad = np.array([False,True,False,True,False,True,False])
+# print(np.sort(sharad))
+
+# sorting the 2-D array
+# import numpy as np
+# sharad = np.array([[3,2,4], [5,0,1]])
+# print(np.sort(sharad,axis=0))
