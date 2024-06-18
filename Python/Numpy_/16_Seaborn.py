e58acb2cb1603d3b2e42a140b9e7d3c8d376f4b3
Manpreet-Singh-1 <mr.mps9999@gmail.com>
2024-06-18 17:09:18 +0530

    commit :)

--- Python/Numpy_/16_Seaborn.py
+++ Python/Numpy_/16_Seaborn.py
@@ -2,10 +2,10 @@
 # seaborn is a library that uses matplotlin underneath to plot graph i.r pyplot.
 # Distplot - distribution plot(curve plot- histogram)
 
-import matplotlib.pyplot as plt
-import seaborn as sns 
-sns.distplot([0,1,2,3,4,5])
-plt.show()
+# import matplotlib.pyplot as plt
+# import seaborn as sns 
+# sns.distplot([0,1,2,3,4,5])
+# plt.show()
 
 # plotting a distplot without the histrogram
 import matplotlib.pyplot as plt
--- Python/Numpy_/17_Normal_Distribution.py
+++ Python/Numpy_/17_Normal_Distribution.py
@@ -13,9 +13,9 @@ sharad = random.normal(loc=1, scale=2,size=(2,3))
 print(sharad)
 
 # # visualization of normal distribution
-# from numpy import random
-# import matplotlib.pyplot as plt
-# import seaborn as sns
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # sns.distplot(random.normal(size=1000), hist=False)
 # plt.show()
